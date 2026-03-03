e1cfadabad743ec54f60537e6bf93b40b7f705ac
igor <igor@igor.io>
2026-03-04 00:21:00 +0100

    rp2350: finalize vfs stream test setup

--- sapi/rp2350/main.php
+++ sapi/rp2350/main.php
@@ -11,23 +11,47 @@ while (true) {
         print "logo:decode:start\n";
         $logo = php_logo_data();
         print "logo:decode:done\n";
-        print "epd:render\n";
-        if ($logo !== false) {
-            $buf = $logo[0];
-            $w = $logo[1];
-            $h = $logo[2];
-            mcu_epd_render($buf, $w, $h);
-        } else {
-            print "epd:logo-decode-fail\n";
-        }
+        // print "epd:render\n";
+        // if ($logo !== false) {
+        //     $buf = $logo[0];
+        //     $w = $logo[1];
+        //     $h = $logo[2];
+        //     mcu_epd_render($buf, $w, $h);
+        // } else {
+        //     print "epd:logo-decode-fail\n";
+        // }
         $did_epd = true;
     }
 
+    $fp = fopen('/lib.php', 'rb');
+    if ($fp === false) {
+        print "fopen-fail\n";
+    } else {
+        $probe = fread($fp, 1);
+        $ch = fgetc($fp);
+        fclose($fp);
+        if ($probe === false) {
+            print "fread-fail\n";
+        } else if ($probe === '') {
+            print "fread-empty\n";
+        } else {
+            print "fread-ok:";
+            print ord($probe);
+            print "\n";
+        }
+        if ($ch === false) {
+            print "fgetc-fail\n";
+        } else {
+            print "fgetc-ok:";
+            print ord($ch);
+            print "\n";
+        }
+    }
     $lib = file_get_contents('/lib.php');
     if ($lib === false) {
-        print "fgetc-fail\n";
+        print "fgc-fail\n";
     } else {
-        print "fgetc-ok:";
+        print "fgc-ok:";
         print strlen($lib);
         print "\n";
     }
