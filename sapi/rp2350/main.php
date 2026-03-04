5f5fb4720332545bee396aaee306f6c163499b8c
igor <igor@igor.io>
2026-03-04 12:11:39 +0100

    rp2350: toggle text/logo on BTN_C and extend 5x7 font

--- sapi/rp2350/main.php
+++ sapi/rp2350/main.php
@@ -4,7 +4,9 @@ print "req:lib:ok\n";
 require '/logo.php';
 print "req:logo:ok\n";
 
-$did_epd = false;
+$mode_logo = false;
+$prev_mask = 0;
+$needs_redraw = true;
 $next_log_s = time() + 1;
 $led_mask = 0;
 
@@ -17,11 +19,45 @@ while (true) {
 
     $mask = mcu_button_wait($remaining_ms);
     if ($mask !== false) {
+        $c_down = (($mask & (1 << MCU_BTN_C)) !== 0);
+        $c_was_down = (($prev_mask & (1 << MCU_BTN_C)) !== 0);
+        if ($c_down && !$c_was_down) {
+            $mode_logo = !$mode_logo;
+            $needs_redraw = true;
+            print "mode:";
+            print $mode_logo ? "logo\n" : "text\n";
+        }
+        $prev_mask = $mask;
         $led_mask = $mask;
         mcu_led_set(MCU_LED_0, (($led_mask & (1 << MCU_BTN_A)) !== 0));
         mcu_led_set(MCU_LED_1, (($led_mask & (1 << MCU_BTN_B)) !== 0));
         mcu_led_set(MCU_LED_2, (($led_mask & (1 << MCU_BTN_UP)) !== 0));
         mcu_led_set(MCU_LED_3, (($led_mask & (1 << MCU_BTN_DOWN)) !== 0));
+        if ($needs_redraw) {
+            if ($mode_logo) {
+                print "logo:decode:start\n";
+                $logo = php_logo_data();
+                print "logo:decode:done\n";
+                if ($logo !== false) {
+                    $buf = $logo[0];
+                    $w = $logo[1];
+                    $h = $logo[2];
+                    mcu_epd_render($buf, $w, $h);
+                    print "logo:render:done\n";
+                } else {
+                    print "logo:decode:fail\n";
+                }
+            } else {
+                print "text:render:start\n";
+                $buf = mcu_fb_create(MCU_EPD_WIDTH, MCU_EPD_HEIGHT, false);
+                mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 18, 18, 'PHP RP2350', 3, 2);
+                mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 20, 56, 'BUTTONS -> LEDS', 2, 2);
+                mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 20, 80, 'TIME + UART LOOP', 2, 2);
+                mcu_epd_render($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT);
+                print "text:render:done\n";
+            }
+            $needs_redraw = false;
+        }
         continue;
     }
 
@@ -31,20 +67,30 @@ while (true) {
     }
     $next_log_s = $now_s + 1;
 
-    if (!$did_epd) {
-        print "logo:decode:start\n";
-        $logo = php_logo_data();
-        print "logo:decode:done\n";
-        // print "epd:render\n";
-        // if ($logo !== false) {
-        //     $buf = $logo[0];
-        //     $w = $logo[1];
-        //     $h = $logo[2];
-        //     mcu_epd_render($buf, $w, $h);
-        // } else {
-        //     print "epd:logo-decode-fail\n";
-        // }
-        $did_epd = true;
+    if ($needs_redraw) {
+        if ($mode_logo) {
+            print "logo:decode:start\n";
+            $logo = php_logo_data();
+            print "logo:decode:done\n";
+            if ($logo !== false) {
+                $buf = $logo[0];
+                $w = $logo[1];
+                $h = $logo[2];
+                mcu_epd_render($buf, $w, $h);
+                print "logo:render:done\n";
+            } else {
+                print "logo:decode:fail\n";
+            }
+        } else {
+            print "text:render:start\n";
+            $buf = mcu_fb_create(MCU_EPD_WIDTH, MCU_EPD_HEIGHT, false);
+            mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 18, 18, 'PHP RP2350', 3, 2);
+            mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 20, 56, 'BUTTONS -> LEDS', 2, 2);
+            mcu_draw_text($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT, 20, 80, 'TIME + UART LOOP', 2, 2);
+            mcu_epd_render($buf, MCU_EPD_WIDTH, MCU_EPD_HEIGHT);
+            print "text:render:done\n";
+        }
+        $needs_redraw = false;
     }
 
     $fp = fopen('/lib.php', 'rb');
